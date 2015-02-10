4080b7ad6498a72314e31d90673ffaf2d9c4231b
lbross <lbross@pdx.edu>
2015-02-10 01:33:02 +0000

    Release BAGIS-P v1.3; Includes updated contact information on FrmAbout

--- vb/BAGIS_P/trunk/src/BAGIS_P/Forms/FrmAbout.vb
+++ vb/BAGIS_P/trunk/src/BAGIS_P/Forms/FrmAbout.vb
@@ -17,8 +17,9 @@
         messageString = messageString & "Center for Spatial Analysis & Research (CSAR)" & vbCrLf & "Geography, Portland State University" & vbCrLf & vbCrLf
         messageString = messageString & "Funded through Spatial Services 2011-2012 Project" & vbCrLf
         messageString = messageString & "Cooperative Ecosystem Studies Units (CESU) Agreement #68-3A75-4-101" & vbCrLf & vbCrLf
-        messageString = messageString & "NWCC Contact: Jim Marron (jim.marron@por.usda.gov)" & vbCrLf
-        messageString = messageString & "PSU Contact: Jiunn-Der (Geoffrey) Duh (jduh@pdx.edu)" & vbCrLf
+        messageString = messageString & "Contacts:" & vbCrLf
+        messageString = messageString & "USDA-NRCS NWCC Water and Climate Services Team: Cara McCarthy (cara.s.mccarthy@por.usda.gov)" & vbCrLf
+        messageString = messageString & "PSU Geography Department: Geoffrey Duh (jduh@pdx.edu)" & vbCrLf
         messageString = messageString & "ArcGIS Programmer: Lesley Bross"
         LblAbout.Text = messageString
     End Sub
